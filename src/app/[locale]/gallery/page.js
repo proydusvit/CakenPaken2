0fdc4328d9644d88cb20b66a756f2866fe3c4381
Vlad Shestavetkyi <95980041+proydusvit@users.noreply.github.com>
2024-01-24 21:23:04 +0200

    add

--- src/app/[locale]/gallery/page.js
+++ src/app/[locale]/gallery/page.js
@@ -2,9 +2,23 @@ import Category from '@/blocks/Gallery/Category';
 import { getTranslations } from 'next-intl/server';
 export async function generateMetadata({ params: { locale } }) {
   const t = await getTranslations({ locale, namespace: 'Gallery' });
-
   return {
     title: t('gallery'),
+    openGraph: {
+      title: t('gallery'),
+      description: 'Гелерея CakenPaken',
+      url: `www.cakenpaken/about`,
+      siteName: 'CakenPaken',
+      images: [
+        {
+          url: '../../../assets/imgAbout/Image_About.webp',
+          width: 192,
+          height: 192,
+        },
+      ],
+      locale: locale,
+      type: 'website',
+    },
   };
 }
 const Gallery = () => {
