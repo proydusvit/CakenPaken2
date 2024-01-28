c581f12b4682b2e82c08a0b71ec9006137506374
Vlad Shestavetkyi <95980041+proydusvit@users.noreply.github.com>
2024-01-28 17:11:51 +0200

    add

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -13,8 +13,8 @@ import Support from '@/blocks/layout/SupportAndDonate/SupportAndDonate';
 export const metadata = {
   title: 'CakenPaken',
   description: 'CakenPaken',
-  viewport:
-    'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no',
+  // viewport:
+  //   'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no',
   icons: [
     {
       rel: 'icon',
@@ -58,19 +58,12 @@ export default function LocaleLayout({ children, params }) {
           dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteJsonLd) }}
           key="website-jsonld"
         />
-        {/* <meta property="og:title" content="Link preview title" />
-        <meta property="og:image" content="Link preview image URL" />
-        <meta property="og:description" content="Link preview description" />
-        <meta property="og:url" content="Canonical link preview URL" /> */}
+
         <meta
           name="google-site-verification"
           content="Oj3W6yNvEyIBpdkspMFsIineMFPdMQiVK3Hr-eeZasg"
         />
-        {/* <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap"
-          async
-        /> */}
+
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -96,18 +89,6 @@ export default function LocaleLayout({ children, params }) {
             href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap"
           />
         </noscript>
-
-        {/* <link
-          rel="stylesheet"
-          type="text/css"
-          charSet="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        /> */}
       </head>
 
       <body>
@@ -121,11 +102,11 @@ export default function LocaleLayout({ children, params }) {
           support={t('support')}
           donate={t('donate')}
         />
-
         <Support support={t('support')} donate={t('donate')} />
         <main>{children}</main>
         <Footer />
         <Analytics />
+
         <SpeedInsights />
       </body>
     </html>
