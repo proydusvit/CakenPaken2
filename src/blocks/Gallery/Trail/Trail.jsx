0bbe924c2eda08ebc55ac21cdd47f3f87bcdba87
Vlad Shestavetkyi <95980041+proydusvit@users.noreply.github.com>
2024-02-20 16:25:06 +0200

    add

--- src/blocks/Gallery/Trail/Trail.jsx
+++ src/blocks/Gallery/Trail/Trail.jsx
@@ -16,7 +16,7 @@ const GalleryTrail = () => {
         '@type': 'ListItem',
         position: '1',
         item: {
-          '@id': `cakenpaken`,
+          '@id': `/`,
           name: t('home'),
         },
       },
@@ -24,7 +24,7 @@ const GalleryTrail = () => {
         '@type': 'ListItem',
         position: '2',
         item: {
-          '@id': `cakenpaken/gallery`,
+          '@id': `gallery`,
           name: t('gallery'),
         },
       },
@@ -32,7 +32,7 @@ const GalleryTrail = () => {
         '@type': 'ListItem',
         position: '3',
         item: {
-          '@id': `cakenpaken/gallery/trail`,
+          '@id': `gallery/trail`,
           name: t('trail'),
         },
       },
