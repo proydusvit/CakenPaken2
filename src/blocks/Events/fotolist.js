de31673cf1ba8a7a724c8bf44585c9f369f17102
Vlad Shestavetkyi <95980041+proydusvit@users.noreply.github.com>
2024-03-01 12:28:56 +0200

    add

---
+++ src/blocks/Events/fotolist.js
@@ -0,0 +1,51 @@
+import img1 from '@/assets/imgEvents/img2.webp';
+import img2 from '@/assets/imgEvents/img3.webp';
+import img3 from '@/assets/imgEvents/img4.webp';
+import img4 from '@/assets/imgEvents/img5.webp';
+// import img5 from '@/assets/imgEvents/img6.webp';
+import img6 from '@/assets/imgEvents/img7.webp';
+
+export const listItems = [
+  {
+    id: 1,
+    img: img1,
+    alt: 'foto',
+    width: 590,
+    heigth: 350,
+  },
+  {
+    id: 2,
+    img: img2,
+    alt: 'foto2',
+    width: 590,
+    heigth: 350,
+  },
+  {
+    id: 3,
+    img: img3,
+    alt: 'foto3',
+    width: 590,
+    heigth: 350,
+  },
+  {
+    id: 6,
+    img: img6,
+    alt: 'foto3',
+    width: 590,
+    heigth: 350,
+  },
+  //   {
+  //     id: 4,
+  //     img: img4,
+  //     alt: 'foto',
+  //     width: 590,
+  //     heigth: 630,
+  //   },
+  //   {
+  //     id: 5,
+  //     img: img5,
+  //     alt: 'foto2',
+  //     width: 330,
+  //     heigth: 330,
+  //   },
+];
